5e6e0cb05b77686069d67dd0e259580dd5ace7e4
Ananya <72216605+ananyachibber21@users.noreply.github.com>
2021-05-28 08:38:06 +0530

    Update Q4.java
    Print the sum, difference and product of two complex numbers by creating a class named 'Complex' with separate methods for each operation whose real and imaginary parts are entered by the user.

--- Practical4-Objects_and_Classes/Q4.java
+++ Practical4-Objects_and_Classes/Q4.java
@@ -1,38 +1,65 @@
 import java.util.*;
 
-public class ComplexNumber{
+public class Complex{
 
    double real, img;
 	
-   ComplexNumber(double r, double i){
+   Complex(double r, double i){
 	this.real = r;
 	this.img = i;
    }
 	
-   public static ComplexNumber sum(ComplexNumber c1, ComplexNumber c2)
+   public static Complex sum(Complex c1, Complex c2)
    {
 	
-        ComplexNumber temp = new ComplexNumber(0, 0);
+        Complex temp = new Complex(0, 0);
 
         temp.real = c1.real + c2.real;
         temp.img = c1.img + c2.img;
         
         return temp;
     }
+
+    public static Complex diff(Complex c1, Complex c2)
+   {
+	
+        Complex temp1 = new Complex(0, 0);
+
+        temp1.real = c1.real - c2.real;
+        temp1.img = c1.img - c2.img;
+        
+        return temp1;
+    }
+
+    public static Complex mult(Complex c1, Complex c2)
+   {
+	
+        Complex temp2 = new Complex(0, 0);
+
+        temp2.real = c1.real * c2.real;
+        temp2.img = c1.img * c2.img;
+        
+        return temp2;
+    }
+
     public static void main(String args[]) {
     	Scanner console = new Scanner(System.in);
 	    System.out.println("Enter real part: ");
         double r1 = console.nextInt();
         System.out.println("Enter imaginary part: ");
         double i1 = console.nextInt();
-        ComplexNumber c1 = new ComplexNumber(r1,i1);
+        Complex c1 = new Complex(r1,i1);
 		
 		System.out.println("Enter real part: ");
         double r2 = console.nextInt();
         System.out.println("Enter imaginary part: ");
         double i2 = console.nextInt();
-        ComplexNumber c2 = new ComplexNumber(r2,i2);
-        ComplexNumber temp = sum(c1, c2);
-        System.out.printf("Sum is: "+ temp.real+" + "+ temp.img +"i");
+        Complex c2 = new Complex(r2,i2);
+        Complex temp = sum(c1, c2);
+        Complex temp1 = diff(c1, c2);
+        Complex temp2 = mult(c1, c2);
+        System.out.println("Sum is: "+ temp.real+" + "+ temp.img +"i");
+        System.out.println("Sum is: "+ temp1.real+" + "+ temp1.img +"i");
+        System.out.println("Sum is: "+ temp2.real+" + "+ temp2.img +"i");
     }
 }
