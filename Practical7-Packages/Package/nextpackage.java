1d7df44b1291629fe960696384c6d2b02829dde8
Ananya <72216605+ananyachibber21@users.noreply.github.com>
2021-06-05 22:55:29 +0530

    Create nextpackage.java

---
+++ Practical7-Packages/Package/nextpackage.java
@@ -0,0 +1,11 @@
+package Exercises;
+import Exercises.Java.*;
+
+public class nextPackage
+{
+	public static void main(String args[])
+	{
+		packageDemo ob = new packageDemo();
+		ob.display();
+	}
+}
