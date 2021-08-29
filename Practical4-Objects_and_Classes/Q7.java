75d5cd84d4d5c360c853ab2a8937660440543fb9
Ananya <72216605+ananyachibber21@users.noreply.github.com>
2021-08-29 22:27:21 +0530

    Heart Rate Calculator
    Create a class called HeartRates. The class attributes should include the person’s first name, last name and date of birth (consisting of separate attributes for the month, day and year of birth). The class should have a constructor that receives this data as parameters. For each attribute provide set and get methods. The class also should include the following methods:
    a)	a method that calculates and returns the person’s age (in years)
    b)	a method that calculates and returns the person’s maximum heart rate (the formula for calculating your maximum heart rate in beats per minute is 220 minus your age in Years) 
    c)	a method that calculates and returns the person’s target heart rate (target heart rate is a range that’s 50–85% of your maximum heart rate.

---
+++ Practical4-Objects_and_Classes/Q7.java
@@ -0,0 +1,129 @@
+import java.util.Scanner;
+
+class HeartRates {
+
+    private String firstName;
+    private String lastName;
+    private int month;
+    private int day;
+    private int year;
+
+    // constructor
+    public HeartRates( String fName, String lName, int aMonth,
+        int aDay, int aYear) {
+
+        firstName = fName;
+        lastName = lName;
+        month = aMonth;
+        day = aDay;
+        year = aYear;
+    }
+
+    // method to set first name
+    public void setFirstName( String fName ) {
+        firstName = fName;
+    }
+
+    // method to get first name
+    public String getFirstName() {
+        return firstName;
+    } 
+
+    // method to set last name
+    public void setLastName( String lName ) {
+        lastName = lName;
+    }
+
+    // method to get last name
+    public String getLastName() {
+        return lastName;
+    }
+
+    // method to set month
+    public void setMonth( int aMonth ) {
+        month = aMonth;
+    }
+
+    // method to get month
+    public int getMonth() {
+        return month;
+    }
+
+    // method to set day
+    public void setDay( int aDay ) {
+        day = aDay;
+    }
+
+    // method to get day
+    public int getDay() {
+        return day;
+    }
+
+    // method to set year
+    public void setYear( int aYear ) {
+        year = aYear;
+    }
+
+    // method to get year
+    public int getYear() {
+        return year;
+    }
+
+    // returns person's age
+    public int ageInYears() {
+        return 2018 - getYear();
+    }
+
+    // returns maximum heart rate
+    public int maxHeartRate() {
+        return 220 - ageInYears();
+    } 
+
+    // display target heart rate
+    public String targetHeartRate() {
+    // targetHeartRate = range 50% - 85% of maxheartrate
+        double fifty = 0.5 * maxHeartRate();
+        double eightyFive = 0.85 * maxHeartRate();
+        String target = (int)fifty + " BPM - " + (int)eightyFive + " BPM\n"; 
+        return target;
+    }
+
+} // end class HeartRates
+
+public class Q7 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String firstName;
+        String lastName;
+        int month;
+        int day;
+        int year;
+
+        // collecting user's information
+        System.out.print("Enter your first name: ");
+        firstName = input.nextLine(); // no need of setFirstName()
+
+        System.out.print("Enter your last name: ");
+        lastName = input.nextLine(); // no need of setLastName()
+
+        System.out.print("Enter your date of birth(month day year): ");
+        month = input.nextInt(); // no need of setMonth()
+        day = input.nextInt();   // no need of setDay()
+        year = input.nextInt();  // no need of setYear()
+
+        /* Object of HeartRates is defined after reading all values */
+        HeartRates profile = new HeartRates(firstName, lastName, month, day, year); // calling constructor with arguments
+
+        // displaying user's information
+        System.out.printf("\nFirst Name: %s\n", profile.getFirstName());
+        System.out.printf("Last Name: %s\n", profile.getLastName());
+        System.out.printf("Date of birth: %d\\%d\\%d\n", profile.getMonth(), profile.getDay(), profile.getYear());
+        System.out.printf("Age: %d\n", profile.ageInYears());
+        System.out.printf("Maximum heart rate: %d BPM\n", profile.maxHeartRate());
+        System.out.printf("Target heart rate: " + profile.targetHeartRate());
+
+    } // end method main
+} // end class HeartRateTest
