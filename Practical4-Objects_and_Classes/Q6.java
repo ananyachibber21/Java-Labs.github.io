7c8af61eaac269503dcc9efa588e6ccf454e0388
Ananya <72216605+ananyachibber21@users.noreply.github.com>
2021-08-29 13:34:09 +0530

    Adding Two Distance
    Write a program to read two distances in feet and inches and find their sum.  
    
    Definition of Done: 
    a) Two java files to be defined. One for class definitions and another for the application 
    b) A class distance is defined with two float variables for feet and inch and the following functionality: 
    i. Non-parametrized and parameterized constructors are defined. 
    ii. Get and set methods are defined for all the instance variables. 
    iii. Sum function is defined to calculate the sum of the two distance variables. 
    iv. Display function is defined with width of 7 and precision of 2.

---
+++ Practical4-Objects_and_Classes/Q6.java
@@ -0,0 +1,70 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.util.*;
+
+class Distance
+{
+    private int feet;
+    private int inches;
+    
+    public void getDistance()
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.print("Enter feet: ");
+        feet=sc.nextInt();
+        System.out.print("Enter inches: ");
+        inches=sc.nextInt();
+    }
+    public void showDistance()
+    {
+        System.out.println("Feet: "+ feet + "\tInches: "+ inches);
+    }
+    
+    public void addDistance(Distance D1, Distance D2)
+    {
+        inches=D1.inches+D2.inches;
+        feet=D1.feet+D2.feet+(inches/12);
+        inches=inches%12;
+    }
+}
+
+public class Q6
+{
+    public static void main(String []s)
+    {
+        
+        
+
+            Distance D1=new Distance();
+            Distance D2=new Distance();
+            Distance D3=new Distance();
+            
+            System.out.println("Enter first distance: ");
+            D1.getDistance();
+            
+            System.out.println("Enter second distance: ");
+            D2.getDistance();
+            
+            D3.addDistance(D1,D2);
+
+            System.out.println("Total distance is:" );
+            D3.showDistance();
+        
+        
+    }
+} 
